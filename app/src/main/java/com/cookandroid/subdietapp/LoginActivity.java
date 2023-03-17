976737986c81a57e822003a2f5b22be2fcc9f85b
yugyeong1 <dbrud1032@gmail.com>
2023-03-17 23:26:46 +0900

    xml 개발

--- app/src/main/java/com/cookandroid/subdietapp/LoginActivity.java
+++ app/src/main/java/com/cookandroid/subdietapp/LoginActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView txtRegister;
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         txtRegister = findViewById(R.id.txtRegister);
+        btnLogin = findViewById(R.id.btnLogin);
 
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,8 +28,17 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, RegisterFirstActivity.class);
                 startActivity(intent);
 
-                finish();
 
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
             }
         });
 
--- app/src/main/java/com/cookandroid/subdietapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/subdietapp/MainActivity.java
@@ -1,18 +1,81 @@
 package com.cookandroid.subdietapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
+    // 각 프레그먼트를 멤버변수로 만든다.
+    public Fragment firstFragment;
+    Fragment secondFragment;
+    Fragment thirdFragment;
+    Fragment forthFragment;
+
+
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+        FragmentTransaction mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(intent);
+
+
+        navigationView = findViewById(R.id.bottomNavigationView);
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        thirdFragment = new ThirdFragment();
+        forthFragment = new forthFragment();
+
+
+        // 탭바를 누르면 동작하는 코드
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                fragment = null;
+
+                if (itemId == R.id.firstFragment){
+                    fragment = firstFragment;
+
+                } else if (itemId == R.id.secondFragment){
+                    fragment = secondFragment;
+                } else if (itemId == R.id.thirdFragment) {
+                    fragment = thirdFragment;
+                } else if (itemId == R.id.forthFragment) {
+                    fragment = forthFragment;
+                }
+                return loadFragment(fragment);
+            }
+        });
+
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            // 화면 바뀌게 하는 코드
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+            return true;
+        } else {
+            return false;
+        }
     }
 }
