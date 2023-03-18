725a670e195396ce8344f4c9e34dc21c4860971f
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2023-03-19 02:46:24 +0900

    칼로리xml

---
+++ app/src/main/java/com/cookandroid/subdietapp/SelectedDinnerActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.subdietapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedDinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_dinner);
+    }
+}
