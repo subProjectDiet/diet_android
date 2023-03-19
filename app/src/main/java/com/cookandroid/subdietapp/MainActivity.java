2347b1b4839683bb5106ad32ec480689f975f5c1
yugyeong1 <dbrud1032@gmail.com>
2023-03-19 20:17:13 +0900

    메인화면 xml 개발

--- app/src/main/java/com/cookandroid/subdietapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/subdietapp/MainActivity.java
@@ -1,13 +1,11 @@
 package com.cookandroid.subdietapp;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -30,9 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction mFragmentTransaction = getSupportFragmentManager().beginTransaction();
-
-//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        //        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
 //        startActivity(intent);
 
 
