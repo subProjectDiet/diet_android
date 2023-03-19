5c99c391f7c1c8d09db474349f2625268f0f8a77
yugyeong1 <dbrud1032@gmail.com>
2023-03-19 21:52:25 +0900

    add

---
+++ app/src/main/java/com/cookandroid/subdietapp/ExerciseSearchAddActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.subdietapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExerciseSearchAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_search_add);
+    }
+}
---
+++ app/src/main/java/com/cookandroid/subdietapp/ExerciseSearchEditActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.subdietapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseSearchEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_search_edit);
+    }
+}
