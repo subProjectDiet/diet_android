faaa40ab9b27d904970a215c5585c10b816d13b3
yugyeong1 <dbrud1032@gmail.com>
2023-03-19 15:39:37 +0900

    add

--- app/src/main/java/com/cookandroid/subdietapp/FirstFragment.java
+++ app/src/main/java/com/cookandroid/subdietapp/FirstFragment.java
@@ -1,12 +1,11 @@
 package com.cookandroid.subdietapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +22,7 @@ public class FirstFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public FirstFragment() {
         // Required empty public constructor
@@ -60,5 +60,7 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+
+
     }
 }
